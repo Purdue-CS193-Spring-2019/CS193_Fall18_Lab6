5e1f2bd3e3e446a81bcc31e6144b3b6d3600c147
Yusong Ming <charliem@Yusongs-MBP.local>
2019-04-03 23:36:52 -0400

    some changes

--- lab6.cpp
+++ lab6.cpp
@@ -2,14 +2,9 @@
 
 int main()
 {
-<<<<<<< HEAD
   std::cout << "Hello, Purdue CS!\n";
   std::cout << "Hello, Purdue CS!\n";
   std::cout << "Hello, Purdue CS!\n";
-=======
-  std::cout << "Hello, World!\n";
-  std::cout << "Hello, World!\n";
-  std::cout << "Hello, World!!!\n";
->>>>>>> printThreeTimes
+
   return 0;
 }
